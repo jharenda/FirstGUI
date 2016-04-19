afaef61ea4f53056045664ade4479bf061c7c350
Jennifer <Jennifer@10.30.11.147>
2016-04-19 14:17:49 -0500

    change to Converter service

--- src/TemperatureConverter/MainWindow.java
+++ src/TemperatureConverter/MainWindow.java
@@ -5,17 +5,23 @@
  */
 package TemperatureConverter;
 
+import java.util.Enumeration;
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+
 /**
  *
  * @author Jennifer
  */
 public class MainWindow extends javax.swing.JFrame {
-private TemperatureConverterService convertService; 
+
+    private TemperatureConverterService convertService;
+
     /**
      * Creates new form MainWindow
      */
     public MainWindow() {
-        
+
         initComponents();
         convertService = new TemperatureConverterService();
     }
@@ -29,23 +35,29 @@ private TemperatureConverterService convertService;
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lblMessage = new javax.swing.JLabel();
+        rdoGroupTempConversions = new javax.swing.ButtonGroup();
+        lblInstructions = new javax.swing.JLabel();
         txtInput = new javax.swing.JTextField();
         btnConvert = new javax.swing.JButton();
         txtCelciusOutput = new javax.swing.JTextField();
-        lblMessage1 = new javax.swing.JLabel();
+        lblConvertedOutput = new javax.swing.JLabel();
+        rdoFartoCel = new javax.swing.JRadioButton();
+        rdoCeltoFar = new javax.swing.JRadioButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(204, 204, 255));
 
-        lblMessage.setFont(new java.awt.Font("Berlin Sans FB", 1, 18)); // NOI18N
-        lblMessage.setText("Enter temperature in Fahrenheit:");
+        lblInstructions.setFont(new java.awt.Font("Berlin Sans FB", 1, 18)); // NOI18N
+        lblInstructions.setForeground(new java.awt.Color(204, 51, 0));
+        lblInstructions.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblInstructions.setText("Enter temperature to be converted:");
 
         txtInput.setFont(new java.awt.Font("Berlin Sans FB", 1, 18)); // NOI18N
 
         btnConvert.setBackground(new java.awt.Color(255, 102, 0));
         btnConvert.setFont(new java.awt.Font("Berlin Sans FB", 1, 18)); // NOI18N
-        btnConvert.setForeground(new java.awt.Color(255, 102, 0));
+        btnConvert.setForeground(new java.awt.Color(204, 51, 0));
         btnConvert.setText("Convert");
         btnConvert.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -56,61 +68,141 @@ private TemperatureConverterService convertService;
         txtCelciusOutput.setFont(new java.awt.Font("Berlin Sans FB", 1, 18)); // NOI18N
         txtCelciusOutput.setForeground(new java.awt.Color(255, 102, 0));
 
-        lblMessage1.setFont(new java.awt.Font("Berlin Sans FB", 1, 18)); // NOI18N
-        lblMessage1.setText("The temperature in Celcius is: ");
+        lblConvertedOutput.setFont(new java.awt.Font("Berlin Sans FB", 1, 18)); // NOI18N
+        lblConvertedOutput.setForeground(new java.awt.Color(204, 51, 0));
+        lblConvertedOutput.setText("Result: ");
+
+        rdoGroupTempConversions.add(rdoFartoCel);
+        rdoFartoCel.setFont(new java.awt.Font("Berlin Sans FB", 0, 13)); // NOI18N
+        rdoFartoCel.setText("Fahrenheit to Celcius");
+        rdoFartoCel.setToolTipText("");
+        rdoFartoCel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rdoFartoCelActionPerformed(evt);
+            }
+        });
+
+        rdoGroupTempConversions.add(rdoCeltoFar);
+        rdoCeltoFar.setFont(new java.awt.Font("Berlin Sans FB", 0, 13)); // NOI18N
+        rdoCeltoFar.setText("Celcius to Fahrenheit");
+
+        jButton1.setFont(new java.awt.Font("Berlin Sans FB", 1, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(204, 51, 0));
+        jButton1.setText("Exit");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btnConvert)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addContainerGap()
-                            .addComponent(lblMessage1, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(layout.createSequentialGroup()
-                            .addContainerGap()
-                            .addComponent(lblMessage, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addGap(18, 18, 18)
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(txtInput, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtCelciusOutput, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(26, Short.MAX_VALUE))
+                    .addComponent(lblInstructions, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(73, 73, 73)
+                        .addComponent(lblConvertedOutput, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtInput, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(rdoFartoCel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 144, Short.MAX_VALUE)
+                            .addComponent(rdoCeltoFar, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtCelciusOutput))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnConvert, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblMessage, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnConvert))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(txtInput, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addComponent(lblInstructions, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtCelciusOutput, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lblMessage1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(66, 66, 66))
+                .addComponent(txtInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(5, 5, 5)
+                .addComponent(rdoFartoCel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnConvert)
+                    .addComponent(rdoCeltoFar, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtCelciusOutput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblConvertedOutput, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addGap(99, 99, 99))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnConvertActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConvertActionPerformed
-       String fahrenheit = txtInput.getText();
-      String result = convertService.convertFahrenheitToCelsius(fahrenheit);
-      txtCelciusOutput.setText(result);
+        String result = null;
+
+        String selected = getSelectedButtonText(rdoGroupTempConversions);
+        try {
+        if (selected.equals("Fahrenheit to Celcius")) {
+            result = fahrenheitToCelcius();
+        } else if (selected.equals("Celcius to Fahrenheit")) {
+            result = celciusToFahrenheit();
+        }
+        } catch(IllegalArgumentException e){
+            System.out.println("Illegal Arugment: " + e.getMessage());
+            
+        }
+        dispose();
+       new ResultWindow(result).setVisible(true);
+    
+        txtCelciusOutput.setText(((result)));
+
     }//GEN-LAST:event_btnConvertActionPerformed
+
+    private String getSelectedButtonText(ButtonGroup buttonGroup) {
+        String text = null;
+
+        for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
+            AbstractButton button = buttons.nextElement();
+
+            if (button.isSelected()) {
+                text = button.getText();
+                break;
+            }
+        }
+        return text;
+    }
+    private void rdoFartoCelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rdoFartoCelActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_rdoFartoCelActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        System.exit(0);        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+    private String fahrenheitToCelcius() {
+        String tempInput = txtInput.getText();
+        String result = convertService.convertFahrenheitToCelsius(tempInput);
+        return result;
+    }
+
+    private String celciusToFahrenheit() {
+        String tempInput = txtInput.getText();
+        String result = convertService.convertCelsiustoFahrenheit(tempInput);
+        return result;
+    }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnConvert;
-    private javax.swing.JLabel lblMessage;
-    private javax.swing.JLabel lblMessage1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel lblConvertedOutput;
+    private javax.swing.JLabel lblInstructions;
+    private javax.swing.JRadioButton rdoCeltoFar;
+    private javax.swing.JRadioButton rdoFartoCel;
+    private javax.swing.ButtonGroup rdoGroupTempConversions;
     private javax.swing.JTextField txtCelciusOutput;
     private javax.swing.JTextField txtInput;
     // End of variables declaration//GEN-END:variables
--- src/TemperatureConverter/TemperatureConverterService.java
+++ src/TemperatureConverter/TemperatureConverterService.java
@@ -10,15 +10,41 @@ package TemperatureConverter;
  * @author Jennifer
  */
 public class TemperatureConverterService {
-    public String convertFahrenheitToCelsius(String degreesFahrenheit){
-        
-        double degrees = Double.parseDouble(degreesFahrenheit);
+    /**
+     * 
+     * @param temperature- the temperature, in Fahrenheit, to be converted to Celsius
+     * @return The temperature, converted to Celsius and in String format with 2 decimal places. 
+     */
+    public String convertFahrenheitToCelsius(String temperature) throws IllegalArgumentException{
+        if (temperature == null || temperature.isEmpty()) {
+            throw new IllegalArgumentException("SOrry");
+        }
+        double degrees = Double.parseDouble(temperature);
         
         double conversion =((degrees - 32)/1.8);
+         String formatedTemp = String.format("%.2f", conversion);
+       
         
-        String finalTemp =  Double.toString(conversion);
+        return formatedTemp;
         
-        return finalTemp;
+    }
+    /**
+     * 
+     * @param temperature the temperature, in Celsius, to be converted to Fahrenheit
+     * @return The temperature, converted to Fahrenheit, and in String format with 2 decimal places.
+     */
+        public String convertCelsiustoFahrenheit(String temperature) throws IllegalArgumentException{
+        if (temperature == null || temperature.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        
+        double degrees = Double.parseDouble(temperature);
+        
+        double conversion =((degrees *1.8) + 32);
+        
+       String formatedTemp = String.format("%.2f", conversion);
+        
+        return formatedTemp;
         
     }
 }
